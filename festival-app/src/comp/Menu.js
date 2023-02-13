0428821cb1e973cca7f172d4af59c238ef6c0723
rayansabah <92931727+rayansabah@users.noreply.github.com>
2023-02-13 13:38:00 +0100

    added more option on menu

--- festival-app/src/comp/Menu.js
+++ festival-app/src/comp/Menu.js
@@ -17,9 +17,19 @@ export default function Menu() {
                     <a href="/Scene">Scener</a>
                 </li>
                 <li>
+                    <a href="/Omrade">Område</a>
+                </li>
+                
+                <li>
                     <a href="/Mat">Mat</a>
                 </li>
-
+                
+                <li>
+                    <a href="/Kontakt">Kontakta Oss</a>
+                </li>
+                <li>
+                    <a href="/About">Om oss</a>
+                </li>
             </ul>
 
         </navbar>
