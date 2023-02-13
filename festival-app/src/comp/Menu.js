67629005befda91e2bd87bb60b79f7575814f18e
rayansabah <92931727+rayansabah@users.noreply.github.com>
2023-02-13 13:05:04 +0100

    Added logo

--- festival-app/src/comp/Menu.js
+++ festival-app/src/comp/Menu.js
@@ -1,23 +1,29 @@
 import React from 'react'
+import logo from './logo.png';
 
-export default function Menu () {
-  return (
-         <navbar className="nav">
-  
-      <ul>
-          <li>
-         <a href="/" className="logo"> logo</a>
-         <li>
-             <a href="/Scene">Scener</a>
-         </li>
-         <li>
-             <a href="/Mat">Mat</a>
-         </li>
-     </li>
-  </ul>
-  
-  </navbar>
-  )
-  }
+
+export default function Menu() {
+    return (
+        <navbar className="nav">
+            <ul>
+                <li>
+                    <a href="/" className="logo" >  <img src={logo}  alt="logo" width={270} /></a>
+                </li>
+            </ul>
+
+            <ul>
+
+                <li>
+                    <a href="/Scene">Scener</a>
+                </li>
+                <li>
+                    <a href="/Mat">Mat</a>
+                </li>
+
+            </ul>
+
+        </navbar>
+    )
+}
 
 
