bb53e28162e23a372ed0a3955ce5cdcc5e342633
rayansabah <92931727+rayansabah@users.noreply.github.com>
2023-02-14 22:33:56 +0100

    Added maps in the location page

--- festival-app/src/pages/Location.js
+++ festival-app/src/pages/Location.js
@@ -1,5 +1,31 @@
-export default function Location(){
-    return   <h1>l</h1>
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import '../css/location.css'
 
+const center = [58.07814215851804, 13.020617914330508]
+
+
+export default function Location() {
+    
+    return <div className='rcorners1'>
+        <MapContainer style={{ height: '90%', width:'100%'}}
+            center={center}
+            zoom={13}
+            attributionControl={false}
+            zoomControl={false}>
+            
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={center}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+
+    </div>
 
 }
