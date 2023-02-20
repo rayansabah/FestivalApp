d0d083d9779f3469c22cc8bc4a6052b175140908
rayansabah <92931727+rayansabah@users.noreply.github.com>
2023-02-20 13:23:31 +0100

    Added the favorit funktion

---
+++ festival-app/src/comp/Popuptext.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import '../css/imgPopup.css'
+
+import Star from './Group.svg'
+
+
+
+function Popuptext( { src }) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowPopup(true);
+  };
+
+  const handlePopupClose = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <>
+      <button className="image-popup-button" onClick={handleButtonClick}>
+      <img src={Star} alt="img" />
+      </button>
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-content">
+            {src}
+            <button className="close-button" onClick={handlePopupClose}>
+              ha ett X här
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Popuptext;
--- festival-app/src/pages/Scene.js
+++ festival-app/src/pages/Scene.js
@@ -1,7 +1,12 @@
+import React, { useState } from 'react';
+
 import data from '../jsonTemp/scene.json'
 import Img from '../comp/ImgPopup'
 import '../css/scene.css'
 import Navbar from '../comp/NavBar';
+
+import Popup from '../comp/Popuptext.js'
+import Popuptext from '../comp/Popuptext.js';
 
 
 
@@ -9,12 +14,46 @@ export default function Scene() {
     const scen1 = data.Scenes.find((scene) => scene.name === "Scen 1");
     const scen2 = data.Scenes.find((scene) => scene.name === "Scen 2");
     const scen3 = data.Scenes.find((scene) => scene.name === "Scen 3");
+    const [favorites, setFavorites] = useState([]);
+
+    const handleAddFavorite = (artist, time) => {
+        setFavorites((prevFavorites) => [
+            ...prevFavorites,
+            { artist: artist, time: time },
+        ]);
+    };
+
+    const handleRemoveFavorite = (artist, time) => {
+        setFavorites((prevFavorites) =>
+            prevFavorites.filter(
+                (favorite) => favorite.artist !== artist || favorite.time !== time
+            )
+        );
+    };
+
+    const isFavorite = (artist, time) => {
+        return favorites.some(
+            (favorite) => favorite.artist === artist && favorite.time === time
+        );
+    };
+
+
     return <div >
-        <Navbar/>
-         <div>
+        <Navbar />
+        <div>
             {data.SceneInfo.map(scene => (
                 <div className='scene-square'>
-                    <h1 className='scene-Header'>{scene.name}</h1>
+                    <h1 className='scene-Header'>{scene.name} </h1>
+                    <Popuptext src={<div>
+                        <h2>Favorites</h2>
+                        <ul>
+                            {favorites.map((favorite, index) => (
+                                <li key={index}>
+                                    {favorite.artist} - {favorite.time}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>} />
                     <p>
                         {scene.info.map(item => (
                             <p>{item}</p>
@@ -26,41 +65,98 @@ export default function Scene() {
 
         <div className="scene-square">
             <div className='scene-flex'>
-            <h2>{scen1.name}</h2>
-            <Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0bFnopqoRjdsoRQpSzPixsS2lLVMSsMbxo044VUPQ&s"  /> 
+                <h2>{scen1.name}</h2>
+                <Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0bFnopqoRjdsoRQpSzPixsS2lLVMSsMbxo044VUPQ&s" />
             </div>
             <ul>
                 {scen1.performances.map((performance) => (
                     <li key={performance.artist}>
-                        {performance.artist} - {performance.time}
-                    </li>
-                ))}
-            </ul>
-        </div>
 
-        <div className="scene-square">
-        <div className='scene-flex'>
-            <h2>{scen2.name}</h2>
-            <Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPha0tgqovvWrctey5XsWasNB9qNh88cCMLCTBFEnG9w&s" />  
-            </div>
-            <ul>
-                {scen2.performances.map((performance) => (
-                    <li key={performance.artist}>
                         {performance.artist} - {performance.time}
+
+                        {isFavorite(performance.artist, performance.time) ? (
+                            <button
+                                onClick={() =>
+                                    handleRemoveFavorite(performance.artist, performance.time)
+                                }
+                            >
+                                Remove Favorite
+                            </button>
+                        ) : (
+                            <button
+                                onClick={() =>
+                                    handleAddFavorite(performance.artist, performance.time)
+                                }
+                            >
+                                Add Favorite
+                            </button>
+                        )}
                     </li>
                 ))}
             </ul>
         </div>
         
         <div className="scene-square">
-        <div className='scene-flex'>
-            <h2>{scen3.name}</h2>
-            <Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPha0tgqovvWrctey5XsWasNB9qNh88cCMLCTBFEnG9w&s" />  
+            <div className='scene-flex'>
+                <h2>{scen2.name}</h2>
+                <Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPha0tgqovvWrctey5XsWasNB9qNh88cCMLCTBFEnG9w&s" />
+            </div>
+            <ul>
+                {scen2.performances.map((performance) => (
+                    <li key={performance.artist}>
+
+                        {performance.artist} - {performance.time}
+
+                        {isFavorite(performance.artist, performance.time) ? (
+                            <button
+                                onClick={() =>
+                                    handleRemoveFavorite(performance.artist, performance.time)
+                                }
+                            >
+                                Remove Favorite
+                            </button>
+                        ) : (
+                            <button
+                                onClick={() =>
+                                    handleAddFavorite(performance.artist, performance.time)
+                                }
+                            >
+                                Add Favorite
+                            </button>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+        <div className="scene-square">
+            <div className='scene-flex'>
+                <h2>{scen3.name}</h2>
+                <Img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPha0tgqovvWrctey5XsWasNB9qNh88cCMLCTBFEnG9w&s" />
             </div>
             <ul>
                 {scen3.performances.map((performance) => (
                     <li key={performance.artist}>
+
                         {performance.artist} - {performance.time}
+
+                        {isFavorite(performance.artist, performance.time) ? (
+                            <button
+                                onClick={() =>
+                                    handleRemoveFavorite(performance.artist, performance.time)
+                                }
+                            >
+                                Remove Favorite
+                            </button>
+                        ) : (
+                            <button
+                                onClick={() =>
+                                    handleAddFavorite(performance.artist, performance.time)
+                                }
+                            >
+                                Add Favorite
+                            </button>
+                        )}
                     </li>
                 ))}
             </ul>
